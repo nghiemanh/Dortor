6b4fe52c23e2209ae43e84a6b1c55a66ab618cf6
nghiemanh <47681268+nghiemanh@users.noreply.github.com>
2020-12-17 13:27:28 +0700

    update 4

--- Doctor.sql
+++ Doctor.sql
@@ -4,12 +4,12 @@ USE doctor;
 -- DROP TABLE  doctorlist;
 CREATE TABLE doctorlist (
     id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    NAME NVARCHAR(45) NOT NULL,
-    gender NVARCHAR(45),
+    name NVARCHAR(45) NOT NULL,
+    gender boolean,
     nationality NVARCHAR(45) NOT NULL,
     address NVARCHAR(100) DEFAULT 'xxx',
     phone VARCHAR(20) DEFAULT '+84xxx',
-    eduction NVARCHAR(100) NOT NULL,
+    education NVARCHAR(100) NOT NULL,
     position NVARCHAR(255) NOT NULL,
     specialized NVARCHAR(255) NOT NULL,
     hospital NVARCHAR(100) NOT NULL
@@ -18,10 +18,10 @@ CREATE TABLE doctorlist (
 -- DROP TABLE doctorinfo;
 CREATE TABLE doctorinfo (
     id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    NAME NVARCHAR(45),
+    name NVARCHAR(45),
     address NVARCHAR(100),
     phone VARCHAR(20),
-    eduction NVARCHAR(100),
+    education NVARCHAR(100),
     position NVARCHAR(255),
     hospital NVARCHAR(100),
     reward NVARCHAR(1000),
@@ -35,7 +35,7 @@ CREATE TRIGGER INFO AFTER
 INSERT ON doctorlist
 FOR EACH ROW
 BEGIN
-	INSERT INTO doctorinfo (name,address,phone,eduction,position,hospital)
-		VALUES (new.name, new.address, NEW.phone,NEW.eduction,NEW.position,NEW.hospital);
+	INSERT INTO doctorinfo (name,address,phone,education,position,hospital)
+		VALUES (new.name, new.address, NEW.phone,NEW.education,NEW.position,NEW.hospital);
 END//
 #DELIMITER;
